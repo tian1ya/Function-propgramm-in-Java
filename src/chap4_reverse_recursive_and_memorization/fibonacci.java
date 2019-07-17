2893372679a0394979228a81b55c773bb0db0cce
liuxu <xuxliu@thoughtworks.com>
2019-07-17 23:16:09 +0800

    read some part content of chap04

---
+++ src/chap4_reverse_recursive_and_memorization/fibonacci.java
@@ -0,0 +1,47 @@
+package chap4_reverse_recursive_and_memorization;
+
+import static chap4_reverse_recursive_and_memorization.TailCall.ret;
+import static chap4_reverse_recursive_and_memorization.TailCall.sus;
+
+public class fibonacci {
+
+    static Integer fibonacciV1(Integer n){
+        if (n == 0 || n == 1){
+            return n;
+        }
+        return fibonacciV1(n-1) + fibonacciV1(n-1);
+    }
+    // 第一次写的函数式这样的，但是这确实最最最致命的函数，因为每一个 递归回产生两个 f 函数，
+
+    // 使用 尾递归
+
+    static Integer fibonacciV2(Integer n, Integer acc1, Integer acc2){
+        if (n == 0){
+            return 0;
+        }else if (n==1){
+            return acc1 + acc2;
+        }
+        return fibonacciV2(n-1, acc2, acc2 + acc1);
+    }
+    // 以上的计算，解决V1 的问题，但是还会有栈溢出的问题
+    static TailCall<Integer> fibonacciV3(Integer n, Integer acc1, Integer acc2){
+        if (n == 0){
+            return ret(0);
+        }else if (n==1){
+            return ret(acc1  + acc2);
+        }else {
+            return sus(() -> fibonacciV3(n-1, acc2, acc1 + acc2));
+        }
+    }
+
+    public static void main(String[] args) {
+        // 实现方法几乎是一样的但是呢，并没有出现书中介绍的结果
+
+        // 栈会溢出版本
+        System.out.println(fibonacciV2(5000, 1, 0));
+
+        // 栈不会溢出版本
+        // 完全不懂了  ？？？？？？？？
+        System.out.println(fibonacciV3(5000, 1, 0).eval());
+    }
+}
--- src/chap4_reverse_recursive_and_memorization/recurrent.java
+++ src/chap4_reverse_recursive_and_memorization/recurrent.java
@@ -1,9 +1,85 @@
 package chap4_reverse_recursive_and_memorization;
 
-import java.util.List;
+import common.Function;
 
+import java.util.List;
+import java.util.function.Supplier;
+
+import static chap4_reverse_recursive_and_memorization.TailCall.ret;
+import static chap4_reverse_recursive_and_memorization.TailCall.sus;
 import static common.CollectionUtilities.head;
 import static common.CollectionUtilities.tail;
+
+
+abstract class TailCall<T>{
+
+    // 开始调用下一个
+    public abstract TailCall<T> resume();
+    // 返回结果
+    public abstract T eval();
+    // 决定是 suspend 还是 return
+    public abstract boolean isSuspend();
+
+    static class Return<T> extends TailCall<T>{
+        private final T t;
+        public Return(T t) {
+            this.t = t;
+        }
+
+        @Override
+        public TailCall<T> resume() {
+            throw new IllegalStateException("return has no resume");
+        }
+
+        @Override
+        public T eval() {
+            return t;
+        }
+
+        @Override
+        public boolean isSuspend() {
+            return false;
+        }
+    }
+
+    static class Suspend<T> extends TailCall<T>{
+
+        public Suspend(Supplier<TailCall<T>> resume) {
+            this.resume = resume;
+        }
+
+        private final Supplier<TailCall<T>> resume;
+
+        @Override
+        public TailCall<T> resume() {
+            return resume.get();
+        }
+
+        @Override
+        public T eval() {
+            TailCall<T> integerTailCall = this;
+            while (integerTailCall.isSuspend()){
+                integerTailCall = integerTailCall.resume();
+            }
+            return integerTailCall.eval();
+        }
+
+        @Override
+        public boolean isSuspend() {
+            return true;
+        }
+    }
+
+    public static <T> Return<T> ret(T t){
+        return new Return<>(t);
+    }
+
+    public static <T> Suspend<T> sus(Supplier<TailCall<T>> s){
+        return new Suspend<T>(s);
+    }
+}
+
+
 
 public class recurrent {
     static Integer sumV1(List<Integer> list){
@@ -18,5 +94,30 @@ public class recurrent {
                 : sumV2(tail(list), acc + head(list));
     }
 
+
+
+    static TailCall<Integer> sumV3(int x, int y){
+        return y == 0
+                ? new TailCall.Return<>(x)
+                : new TailCall.Suspend<>(() -> sumV3(x+1,y-1));
+    }
+
+    static TailCall<Integer> sumV4(int x, int y){
+        return y == 0
+                ? ret(x)
+                : sus(() -> sumV4(x + 1, y - 1));
+    }
+
+
+
+    public static void main(String[] args) {
+        TailCall<Integer> integerTailCall = sumV3(3, 100);
+        while (integerTailCall.isSuspend()){
+           integerTailCall = integerTailCall.resume();
+        }
+        System.out.println(integerTailCall.eval());
+        System.out.println(sumV3(3, 100).eval());
+        System.out.println(sumV4(3,100).eval());
+    }
 
 }
